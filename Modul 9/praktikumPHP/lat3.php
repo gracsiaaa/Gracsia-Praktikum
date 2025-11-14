f782e3201b89a0be433cbd5ba2e35b735fc843e4
gracsiaaa <bunshinxoxo11@gmail.com>
2025-11-14 09:48:04 +0700

    latian php perpindahan dari server

---
+++ Modul 9/praktikumPHP/lat3.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Latihan 3 PWEB</title>
+</head>
+<body>
+    <font color ="Red">
+      <?php echo "Tulisan ini berwarna merah"; ?>
+    </font>
+    <?php 
+      echo "<br/> <b> Tulisan dalam bold atau tulisan tebal";
+    ?>
+</body>
+</html>
---
+++ Modul 9/praktikumPHP/lat5.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>LATIHAN 3 PEMROGRAMAN WEB</title>
+</head>
+<body>
+
+<?php
+$nim = "2305101137"; 
+$nama = "Gracsia Andhika"; 
+$angka1 = 10;
+$angka2 = 5;
+
+$hasilpenjumlahan = $angka1 + $angka2;
+
+echo "NIM : " . $nim . "<br/>";
+echo "Nama : " . $nama . "<br/>";
+echo "Hasil Penjumlahan dari " . $angka1 . " dan " . $angka2 . " = " . $hasilpenjumlahan . "<br/>";
+?>
+
+</body>
+</html>
